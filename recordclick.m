7921acc93dbc58b4558f80e8d0bd47f9b982362c
vfedrigo <45501937+vfedrigo@users.noreply.github.com>
2018-12-11 11:13:15 -0800

    Update recordclick.m
    It doesn't have the popup interface yet, but records clicks correctly.
    The positions are still in terms of (multiplier)*screensize, which can definitely be improved.

--- recordclick.m
+++ recordclick.m
@@ -1,20 +1,23 @@
-function [cardChoice] = recordclick(players)
+function [cardChoice] = recordclick(players, cardsize)
 %eventually figure out what the input should be--trial number?
 
 global windo
 [~, clickX, clickY] = GetClicks;
 [screenXpixels, screenYpixels] = Screen('WindowSize', windo);
 
+cardsizeX = cardsize(:,1)
+cardsizeY = cardszie(:,2)
 
-XconditionLeft = ((clickX < (screenXpixels * 0.10 + 90)) && ...
-    (clickX > (screenXpixels * 0.10 - 90)));
-YconditionLeft = ((clickY < (screenYpixels * 0.2 + 125)) && ...
-    (clickX > (screenXpixels * 0.2 - 125)));
 
-XconditionRight = ((clickX < (screenXpixels * 0.24 + 90)) && ...
-    (clickX >(screenXpixels * 0.24 - 90)));
-YconditionRight = ((clickY < (screenYpixels * 0.2 + 125)) && ...
-    (clickX > (screenXpixels * 0.2 - 125)));
+XconditionLeft = ((clickX < (screenXpixels * 0.10 + 0.5*cardsizeX)) && ...
+    (clickX > (screenXpixels * 0.10 - 0.5*cardsizeX)));
+YconditionLeft = ((clickY < (screenYpixels * 0.2 + 0.5*cardsizeY)) && ...
+    (clickX > (screenXpixels * 0.2 - 0.5*cardsizeY)));
+
+XconditionRight = ((clickX < (screenXpixels * 0.24 + 0.5*cardsizeX)) && ...
+    (clickX >(screenXpixels * 0.24 - 0.5*cardsizeX)));
+YconditionRight = ((clickY < (screenYpixels * 0.2 + 0.5*cardsizeY)) && ...
+    (clickX > (screenXpixels * 0.2 - 0.5*cardsizeY)));
 
 if  XconditionLeft + YconditionLeft == 2
     cardChoice = 'left';
@@ -27,6 +30,7 @@ else disp('Click out of range. Please make a valid selection')
 end
 
 %Show some sort of dialogue to confirm the click 
-%This is under construction
+%This is under construction, but currently prints
+%the card selected (left or right) correctly
 
 end
