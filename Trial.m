f74211b237fded7d93531126dff6d1857554c943
LiXiaomin <lixiaomin@lixiaomins-imac.lan>
2019-01-01 12:00:16 -0800

    enable saving data

--- Trial.m
+++ Trial.m
@@ -1,6 +1,22 @@
-function [dstRect, subdstRect] = Trial(players)
+function Trial(userDataPath, gameDataFilename, imgDir, gameConfig)
 %% TODO: pass in this as parameter
 userDataPath = 'data/';
+gameDataFilename = 'gameData.mat';
+imgDir = 'imgs/';
+gameConfig.players = 8;
+gameConfig.gameNum = 5;
+gameConfig.maxPlayersPerRow = 3;
+gameConfig.numCardsPerPlayer = 2;
+gameConfig.sectionMarginRelative = 0.1;
+gameConfig.cardGapRelative = 0.03;
+gameConfig.cardAspectRatio = 1.5;
+gameConfig.cardBorderWidth = 6;
+gameConfig.objectNum = 3;
+gameConfig.objectAspectRatio = 1.0;
+gameConfig.cardConfigs = zeros(gameConfig.players, gameConfig.numCardsPerPlayer, gameConfig.objectNum, gameConfig.gameNum);
+for i = 1 : gameConfig.gameNum
+   gameConfig.cardConfigs(:, :, :, i) = randi(4, gameConfig.players, gameConfig.numCardsPerPlayer, gameConfig.objectNum);
+end
 
 %% Toolbox setup
 %clear workspace and screen
@@ -35,34 +51,37 @@ try
     %% Log in
     loggedIn = false;
     while ~loggedIn
-        [userId, userExist] = renderLoginPage(window, windowWidth, windowHeight, userDataPath);
+        [userId, userExist] = renderLoginPage(window, windowWidth, windowHeight, userDataPath, gameDataFilename);
         if userExist
             loggedIn = renderContinueExperimentConfirmation(window);
             if loggedIn
-               %% Load Existing User data 
+               %% Load existing data
+               load( sprintf('%s/%s/%s', userDataPath, userId, gameDataFilename)); 
             end
         else
             loggedIn = true;
             %% Initialize New User Data
             mkdir(sprintf('%s/%s', userDataPath, userId));
-            
+            currentGameId = 1;
+            userChoice = zeros(1, gameConfig.gameNum);
+            save(sprintf('%s/%s/%s', userDataPath, userId, gameDataFilename), 'gameConfig', 'currentGameId', 'userChoice');
         end
     end
     
-    for gameId = 1 : 5
-        %% Load Game Config
-        % Below assignments are temporary for debugging
-        players = 8   ;
-        maxPlayersPerRow = 3;
-        numCardsPerPlayer = 2;
-        sectionMarginRelative = 0.1;
-        cardGapRelative = 0.03;
-        cardAspectRatio = 1.5;
-        cardBorderWidth = 6;
-        objectNum = 3;
-        objectAspectRatio = 1.0;
-        cardConfig = randi(4, players, numCardsPerPlayer, objectNum);
-        imgDir = 'imgs/';
+    %% Load Game Config
+    players = gameConfig.players;
+    gameNum = gameConfig.gameNum;
+    maxPlayersPerRow = gameConfig.maxPlayersPerRow;
+    numCardsPerPlayer = gameConfig.numCardsPerPlayer;
+    sectionMarginRelative = gameConfig.sectionMarginRelative;
+    cardGapRelative = gameConfig.cardGapRelative;
+    cardAspectRatio = gameConfig.cardAspectRatio;
+    cardBorderWidth = gameConfig.cardBorderWidth;
+    objectNum = gameConfig.objectNum;
+    objectAspectRatio = gameConfig.objectAspectRatio;
+        
+    for gameId = currentGameId : gameNum
+        cardConfig = gameConfig.cardConfigs(:, :, :, gameId);
         %% Render game on screen
         [sectionCenters, sectionWidth, sectionHeight] = getSectionPositions(players, maxPlayersPerRow, windowWidth, windowHeight);
         [cardWidth, cardHeight, cardXPositions, labelYPosition, labelWidth, labelHeight] = getCardParameters(numCardsPerPlayer, cardAspectRatio, cardGapRelative, ...
@@ -80,6 +99,12 @@ try
             cardChoice = recordclick(window, sectionCenters(:, 1), cardXPositions, cardHeight, cardWidth);
         end
         
+        %% Save game data
+        userChoice(gameId) = cardChoice;
+        currentGameId = gameId + 1;
+        save(sprintf('%s/%s/%s', userDataPath, userId, gameDataFilename), 'gameConfig', 'currentGameId', 'userChoice');
+        
+        %% Press enter for next game
         if ~renderGameTable(window, sectionCenters, cardWidth, cardHeight, cardXPositions, cardBorderWidth, ...
                 objectWidth, objectHeight, objectYPositions, cardConfig, imgDir, labelYPosition, labelWidth, labelHeight, ...
                 cardChoice);
@@ -87,10 +112,7 @@ try
         end
     end
     
-    % %This way at the end there'll be a vector with all the choices, can
-    % %compare to the different playing strategies
-    % ResponseVector = cat(2, ResponseVector, cardChoice);
-    % disp(ResponseVector)
+    %% TODO: Ending page after all games finish
    
     sca
     
